166050b7abf0ba0ac9e2ec02899eefbf300a5caf
hak <hyunhak1223@gmail.com>
2020-12-01 16:17:16 +0100

    first commit

---
+++ tshirts/js/scripts.js
@@ -0,0 +1,138 @@
+
+var products = {
+    'white': {
+        
+        'plain': {
+            'unit_price': 5.12,
+            'photo': 'v-white.jpg' 
+        },
+        'printed': {
+            'unit_price': 8.95,
+            'photo': 'v-white-personalized.jpg' 
+        }
+    },
+    
+    'colored': {
+        'plain': {
+            'unit_price': 6.04,
+            'photo': 'v-color.jpg' 
+        },
+        'printed': {
+            'unit_price': 9.47,
+            'photo': 'v-color-personalized.png' 
+        }
+    }
+}
+
+
+// Search params
+
+var search_params = {
+    "quantity": "",
+    "color": "",
+    "quality": "",
+    "style": "",
+}
+
+
+// Additional pricing rules:
+
+// 1. The prices above are for Basic quality (q150). 
+// The high quality shirt (190g/m2) has a 12% increase in the unit price.
+
+// 2. Apply the following discounts for higher quantities: 
+    // 1: above 1.000 units - 20% discount
+    // 2: above 500 units - 12% discount
+    // 3: above 100 units - 5% discount
+
+
+// Solution:
+
+$(function(){
+
+   
+
+function updateParams() {
+    search_params.color = $("#color .option-button.selected").attr('id');
+    search_params.quality = $("#quality .option-button.selected").attr('id');
+    search_params.style = $("#style").val();
+    search_params.quantity = parseInt($("#quantity").val());
+    updateOrderDetails();
+    
+}
+
+function updateOrderDetails() {
+    $(".refresh-loader").show();
+
+    var qualityId = "#" + search_params.quality;
+    var colorId = "#" + search_params.color;
+    var styleSelector = "#style option[value = "+search_params.style+"]";
+    var orderedTotal = calculateTotal();
+    var unitUrl = "img/" + products[search_params.color][search_params.style].photo;
+
+    $("#result-style").html($(styleSelector).text());
+    $("#result-quality").html($(qualityId).text());
+    $("#result-quantity").html(search_params.quantity);
+    $("#result-color").html($(colorId).text());
+    $("#total-price").text(orderedTotal);
+    $("#photo-product").attr("src", unitUrl);
+
+    $(".refresh-loader").hide();
+
+}
+
+function calculateTotal() {
+  var unitPrice = products[search_params.color][search_params.style].unit_price;
+ 
+  console.log(unitPrice)
+    if (search_params.quality == "q190") {
+    unitPrice *= 1.12
+    } 
+
+    var total = unitPrice*search_params.quantity
+    console.log(unitPrice)
+    if (search_params.quantity >=1000) {
+    total *= 0.8
+    } else if (search_params.quantity >=500) {
+    total *= 0.88
+    } else  if  (search_params.quantity >=100) {
+        total *= 0.95
+    }
+    return total.toLocaleString("en-US", {style: "currency", currency: "USD"})
+    
+}
+
+updateParams()
+$("#quantity").change(function() {
+    search_params.quantity = parseInt($("#quantity").val());
+    updateOrderDetails()
+});
+$("#style").change(function() {
+    search_params.style = $("#style").val();
+    updateOrderDetails();
+});
+
+$(".option-button").click(function() {
+    var clickedParam = $(this).parent().attr("id");
+    var childSelector = "#" + clickedParam + " .option-button";
+
+    $(childSelector).removeClass("selected")
+    $(this).addClass("selected")
+
+    var selectedChild = "#" + clickedParam + " .option-button.selected";
+
+    search_params[clickedParam] = $(selectedChild).attr('id');
+    updateOrderDetails();
+});
+    
+});
+
+
+
+
+
+
+
+
+
+
